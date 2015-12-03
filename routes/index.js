6670f9908fc421171c722aeef0ddeae24900e1fa
nathan-siu <nathansiu9730@gmail.com>
2015-12-03 11:25:22 -0500

    made createShortAnswer view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*get the short answer page*/
+router.get('/shortAnswer', function(req, res, next) {
+  res.render('shortAnswer', { title: 'Short Answer' });
+});
+
 module.exports = router;

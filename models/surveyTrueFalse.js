7b7f90529aa43b56b4130be0822ca0bd4c49e594
nathan-siu <nathansiu9730@gmail.com>
2015-12-10 10:39:27 -0500

    updated truefalse model

--- models/surveyTrueFalse.js
+++ models/surveyTrueFalse.js
@@ -3,8 +3,17 @@ var Schema = mongoose.Schema;// alias for mongoose.Schema
 
 var TrueFalseSchema = new Schema({
 	surveyQuestion: String,
-	surveyAnswer1: String,
-	surveyAnswer2: String
+	surveyAnswer1: Number,
+	surveyQuestion2: String,
+	surveyAnswer2: Number,
+	surveyQuestion3: String,
+	surveyAnswer3: Number,
+	surveyQuestion4: String,
+	surveyAnswer4: Number,
+	surveyQuestion5: String,
+	surveyAnswer5: Number
+	
+	
 	
 
 }, {

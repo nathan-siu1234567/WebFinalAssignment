3426b583fd7a31b0686c202fae310357f2e6eb18
Christian Simpson <ChristianRSimpson25@gmail.com>
2015-12-15 15:08:03 -0500

    changed surveyTake route

--- routes/surveyTake.js
+++ routes/surveyTake.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('surveyTake', { title: 'Express' });
+router.get('/:id', function(req, res, next) {
+  res.render('surveyTake', { title: 'Survey Take' });
 });
 
 module.exports = router;

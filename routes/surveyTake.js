905040c4c3c3cfb8ea3500afc0edd8cbf1846b7e
Christian Simpson <ChristianRSimpson25@gmail.com>
2015-12-15 15:19:43 -0500

    updated routing fo rsurveyTake

--- routes/surveyTake.js
+++ routes/surveyTake.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Truefalse = require('../models/surveyTrueFalse');
+
+
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  res.render('surveyTake', { title: 'Survey Take' });
+  var id = req.params.id;
+  Truefalse
+
+
+  res.render('surveyTake', { 
+  	title: 'Survey Take' 
+  });
 });
 
 module.exports = router;

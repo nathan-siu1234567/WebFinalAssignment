63eea36feb872ef63e72a6085ae5fb00d9516413
Christian Simpson <ChristianRSimpson25@gmail.com>
2015-12-15 15:23:18 -0500

    added more to surveytake ejs

--- routes/surveyTake.js
+++ routes/surveyTake.js
@@ -7,11 +7,14 @@ var Truefalse = require('../models/surveyTrueFalse');
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
-  Truefalse
+  Truefalse.findbyid(id, function(err, survey){
+
+  })
 
 
   res.render('surveyTake', { 
-  	title: 'Survey Take' 
+  	title: 'Survey Take',
+  	survey: survey 
   });
 });
 

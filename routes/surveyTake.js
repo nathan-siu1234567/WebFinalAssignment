600fe94c54522b44e9f935aa0cde35c3961c2324
Christian Simpson <ChristianRSimpson25@gmail.com>
2015-12-15 15:39:22 -0500

    changed surveyTake.ejs

--- routes/surveyTake.js
+++ routes/surveyTake.js
@@ -7,14 +7,19 @@ var Truefalse = require('../models/surveyTrueFalse');
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
-  Truefalse.find(id, function(err, survey){
 
-  })
-
-
-  res.render('surveyTake', { 
-  	title: 'Survey Take',
-  	survey: survey 
+  Truefalse.findById(id, function(err, survey){
+  	 // if we have an error
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+        	res.render('surveyTake', { 
+  			title: 'Survey Take',
+  			survey: survey 
+  			});
+  		}
   });
 });
 
